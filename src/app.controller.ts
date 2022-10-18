eaea30dd65dc598d709ed7dab31cc0ca8be7063a
Nikita Golofaev <smile.nikitoz@gmail.com>
2022-10-18 12:46:00 +0300

    fix: добавит лог

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Param, Post, Res } from '@nestjs/common';
+import { Body, Controller, Logger, Param, Post, Res } from '@nestjs/common';
 import { Response } from 'express';
 import { BOT_TOKEN } from './config';
 import { TelegramService } from './modules/telegram/telegram.service';
@@ -6,6 +6,8 @@ import { NotifyService } from './modules/notify/notify.service';
 
 @Controller()
 export class AppController {
+  private readonly logger = new Logger(AppController.name);
+
   constructor(
     private readonly telegramService: TelegramService,
     private readonly notifyService: NotifyService,
@@ -13,6 +15,8 @@ export class AppController {
 
   @Post(`bot${BOT_TOKEN}`)
   async processUpdate(@Body() data: any, @Res() res: Response): Promise<void> {
+    this.logger.debug(data);
+
     await this.telegramService.processUpdate(data);
 
     res.status(200).send();
